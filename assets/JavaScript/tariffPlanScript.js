6ca41f53c4afba7356d347b943dd1cb3097f3e86
David <liid05754@gmail.com>
2023-08-04 21:38:25 +0300

    card-block and channels added

--- assets/JavaScript/tariffPlanScript.js
+++ assets/JavaScript/tariffPlanScript.js
@@ -6,11 +6,14 @@ const navItem = document.querySelectorAll('.nav__item')
 const additionalText = document.querySelectorAll('.additional-text')
 const account = document.querySelector('.account')
 const accountBlock = document.querySelector('.account-block')
+const btnFirst = document.querySelector('.btn-first')
+const btnSecond = document.querySelector('.btn-second')
+const checkbox = document.querySelectorAll('.checkbox')
 
 
 const pointingToMenu = (event) => {
     nav.classList.toggle('active');
-    menuBtn.classList.toggle('active');
+    menuBtn[0].classList.toggle('active');
     additionalText.forEach(item => item.classList.toggle('active'));
 }
 
@@ -41,4 +44,21 @@ navItem.forEach((item, index) => {
 account.addEventListener('click', () => {
     accountBlock.classList.toggle('active');
 });
+
+btnSecond.addEventListener('click', () => {
+    btnSecond.classList.add('button--green');
+    btnFirst.classList.add('button--transparent', 'channels__button-color');
+    btnFirst.classList.remove('button--black');
+})
+btnFirst.addEventListener('click', () => {
+    btnFirst.classList.add('button--black');
+    btnSecond.classList.add('button--transparent', 'channels__button-color');
+    btnSecond.classList.remove('button--green');
+})
+
+checkbox.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+})
 
